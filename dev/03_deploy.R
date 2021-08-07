6f2f8e67dfb7ea0cf95b711a8f77e3ee53e9b102
luceydav <dnl2001@stern.nyu.edu>
2021-08-07 17:28:50 -0400

    small changes

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -1,12 +1,12 @@
 # Building a Prod-Ready, Robust Shiny Application.
-# 
-# README: each step of the dev files is optional, and you don't have to 
-# fill every dev scripts before getting started. 
-# 01_start.R should be filled at start. 
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
 # 02_dev.R should be used to keep track of your development during the project.
 # 03_deploy.R should be used once you need to deploy your app.
-# 
-# 
+#
+#
 ######################################
 #### CURRENT FILE: DEPLOY SCRIPT #####
 ######################################
@@ -19,6 +19,11 @@ devtools::check()
 rhub::check_for_cran()
 
 # Deploy
+
+## Local, CRAN or Package Manager ----
+## This will build a tar.gz that can be installed locally,
+## sent to CRAN, or to a package manager
+devtools::build()
 
 ## RStudio ----
 ## If you want to deploy on RStudio related platforms
